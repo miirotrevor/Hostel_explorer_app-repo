cedd1a5116ef1af3a30af1c4172eb94bd573bc4e
NAKIMULI  SANDRA <nakimulisandra705@gmail.com>
2023-11-29 17:10:32 +0300

    added comment to initalize file

--- initialize.php
+++ initialize.php
@@ -1,3 +1,4 @@
+# this file helps us connect to database. change credentials to appropriate one
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
 if(!defined('base_url')) define('base_url','http://localhost/lodge/');
